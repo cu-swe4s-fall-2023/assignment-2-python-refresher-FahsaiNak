cfaf3d19e1f24b638bdedd9eca6d5b76db6391b4
fahsain <fahsain.nak@gmail.com>
2023-09-11 11:17:12 -0600

    Edited the get_column function to get a list of the values from the defined column position if meets a condition

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,10 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    result = list()
+    with open(file_name,'r') as f:
+        for ind,line in enumerate(f):
+            array = line.rstrip().split(',')
+            query = array[query_column-1]
+            if query == query_value:
+                print(array)
+                result.append(array[result_column-1])
+    return result
