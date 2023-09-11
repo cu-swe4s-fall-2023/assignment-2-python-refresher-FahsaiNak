0140183f6a51e4926f37b0249581d358ad393830
fahsain <fahsain.nak@gmail.com>
2023-09-11 13:55:31 -0600

    Edited the get_column() to use a named argument for the result_column and set a default to 1

--- my_utils.py
+++ my_utils.py
@@ -1,9 +1,9 @@
-def get_column(file_name, query_column, query_value, result_column):
+def get_column(file_name, query_column, query_value, result_column=1):
     result = list()
     with open(file_name,'r') as f:
         for line in f:
             array = line.rstrip().split(',')
             query = array[query_column-1]
             if query == query_value:
-                result.append(array[result_column-1])
+                result.append(float(array[result_column-1]))
     return result
