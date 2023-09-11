5d4671972346c1a0472b5c0141b257f074c2a5ef
fahsain <fahsain.nak@gmail.com>
2023-09-11 13:23:01 -0600

    Updated the call of get_column() with its input variables

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,8 @@
+import my_utils as mt
+
 country='United States of America'
 county_column = 1
 fires_column = 4
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+fires = mt.get_column(file_name, county_column, country, fires_column)
 print(fires)
