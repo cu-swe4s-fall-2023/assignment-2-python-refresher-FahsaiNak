ead5239bfe7f63e3704279b20c3d0973f97ed468
fahsain <fahsain.nak@gmail.com>
2023-09-11 13:19:53 -0600

    Revised the get_column()

--- my_utils.py
+++ my_utils.py
@@ -1,10 +1,9 @@
 def get_column(file_name, query_column, query_value, result_column):
     result = list()
     with open(file_name,'r') as f:
-        for ind,line in enumerate(f):
+        for line in f:
             array = line.rstrip().split(',')
             query = array[query_column-1]
             if query == query_value:
-                print(array)
                 result.append(array[result_column-1])
     return result
