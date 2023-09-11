cb1327f04f718e057b36fc07323835b6d73740c3
fahsain <fahsain.nak@gmail.com>
2023-09-11 13:56:37 -0600

    Updated to run the edited my_utils.py with run.sh

--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,9 @@
 import my_utils as mt
+import sys
 
-country='United States of America'
-county_column = 1
+file_name = sys.argv[1] #'Agrofood_co2_emission.csv'
+country= sys.argv[2] #'United States of America'
+country_column = 1
 fires_column = 4
-file_name = 'Agrofood_co2_emission.csv'
-fires = mt.get_column(file_name, county_column, country, fires_column)
+fires = mt.get_column(file_name, country_column, country, result_column=fires_column)
 print(fires)
