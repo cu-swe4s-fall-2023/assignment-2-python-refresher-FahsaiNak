0c09365e8111282380a7909af20a787ba62e1b8f
fahsain <fahsain.nak@gmail.com>
2023-09-12 10:48:27 -0600

    Improved the code to meet a Style Guide

--- my_utils.py
+++ my_utils.py
@@ -1,6 +1,6 @@
 def get_column(file_name, query_column, query_value, result_column=1):
     result = list()
-    with open(file_name,'r') as f:
+    with open(file_name, 'r') as f:
         for line in f:
             array = line.rstrip().split(',')
             query = array[query_column-1]
