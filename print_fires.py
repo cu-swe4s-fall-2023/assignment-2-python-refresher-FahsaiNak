27fcab22d8e8434a5f21de7cc473cfd9ee5c52f9
fahsain <fahsain.nak@gmail.com>
2023-09-12 10:51:41 -0600

    Improved the code to meet a Style Guide

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,10 @@
 import my_utils as mt
 import sys
 
-file_name = sys.argv[1] #'Agrofood_co2_emission.csv'
-country= sys.argv[2] #'United States of America'
+file_name = sys.argv[1]
+country = sys.argv[2]
 country_column = 1
 fires_column = 4
-fires = mt.get_column(file_name, country_column, country, result_column=fires_column)
+fires = mt.get_column(file_name, country_column, country,
+                      result_column=fires_column)
 print(fires)
