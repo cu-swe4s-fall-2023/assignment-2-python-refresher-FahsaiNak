81c240b59c6f326dd1afa97cc2d3c0e9ebdd8db0
fahsain <fahsain.nak@gmail.com>
2023-09-13 14:35:39 -0600

    Edited: count a number of fire events from if the value is not equal to zero

--- print_fires.py
+++ print_fires.py
@@ -1,10 +1,21 @@
-import my_utils as mt
+import my_utils
 import sys
+from collections import namedtuple
 
-file_name = sys.argv[1]
-country = sys.argv[2]
-country_column = 1
-fires_column = 4
-fires = mt.get_column(file_name, country_column, country,
-                      result_column=fires_column)
-print(fires)
+
+def main(var):
+    fires = my_utils.get_column(var.file_name, var.country_column,
+                                var.country, result_column=var.fires_column)
+    num_fires = len([val for val in fires if val != 0])-1
+    print("A number of", fires[0], "in", country, "is", num_fires)
+
+
+if __name__ == '__main__':
+    country = "United States of America"
+    country_column = 1
+    fires_column = 4
+    file_name = "Agrofood_co2_emission.csv"
+    Args = namedtuple('Args', ['file_name', 'country',
+                               'country_column', 'fires_column'])
+    Info = Args(file_name, country, country_column, fires_column)
+    main(Info)
