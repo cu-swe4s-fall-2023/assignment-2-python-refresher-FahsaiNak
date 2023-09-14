2f100489da5f407753d0cab831f7a154749f17c0
fahsain <fahsain.nak@gmail.com>
2023-09-14 10:16:15 -0600

    Added documentation

--- my_utils.py
+++ my_utils.py
@@ -1,4 +1,20 @@
 def get_column(file_name, query_column, query_value, result_column=1):
+    """
+    Extracts data from a CSV file based on a query and returns a list of values from a specified result column.
+
+    Args:
+    - file_name (str): The name of the CSV file to read.
+    - query_column (int): The column index (1-based) to use as the query condition.
+    - query_value (str): The value to search for in the specified query column.
+    - result_column (int, optional): The column index (1-based) from which to extract values. Default is 1.
+
+    Returns:
+    - result (list): A list of values from the specified result column that match the query condition.
+
+    Note:
+    - The function assumes that the CSV file has a header row.
+    - The column indices are 1-based, where 1 corresponds to the first column.
+    """
     with open(file_name, 'r') as f:
         for ind, line in enumerate(f):
             array = line.rstrip().split(',')
