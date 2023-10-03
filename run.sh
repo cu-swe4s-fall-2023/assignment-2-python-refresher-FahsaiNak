378a2d5e776c083a8350855a2f8598a83a8cec8a
fahsain <fahsain.nak@gmail.com>
2023-10-03 00:38:25 -0600

    edit to run print_fires.py with operation

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ set -u # raise error if variable is unset
 set -o pipefail # fail if any prior step failed
 
 echo "Program is running"
-python print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --fires_column 3
+python print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --fires_column 4
+python print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --fires_column 4 --operation mean
+python print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --fires_column 4 --operation median
+python print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --fires_column 4 --operation std
 python print_fires.py --file_name Agrofood_o2_emission.csv --country "United States of America" --fires_column 4
 python print_fires.py --file_name Agrofood_co2_emission.csv --country "Bermuda" --fires_column 11
 echo "Done"
