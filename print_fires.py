20706ffe33988e2308ffd9e1c2857bcb411a855f
fahsain <fahsain.nak@gmail.com>
2023-10-03 00:37:47 -0600

    add operation flag and run

--- print_fires.py
+++ print_fires.py
@@ -11,7 +11,7 @@ def get_args():
     - argparse.Namespace: An object containing the parsed command-line arguments.
     """
     parser = argparse.ArgumentParser(
-        description='Print a number of fires in a specified country',
+        description='Print a number of fires in a specified country with/without operation',
         prog='print_fires')
     parser.add_argument('--file_name', type=str,
                         help='Name of the file', required=True)
@@ -21,6 +21,8 @@ def get_args():
                         help='The column number of the fires', required=True)
     parser.add_argument('--country_column', type=int, default=1,
                         help='The column number of the country name')
+    parser.add_argument('--operation', type=str, default=None,
+                        help='The operation to perform: mean, median, std')    
     args = parser.parse_args()
     return args
 
@@ -34,8 +36,19 @@ def main():
                                 args.country, result_column=args.fires_column)
     if fires is None:
         sys.exit(1)
-    num_fires = len([val for val in fires[:-1] if val != 0])
-    print(f"A number of fires from {fires[-1]} in {args.country} is {num_fires}")
+    else:
+        if args.operation == None:
+            val = fires
+        elif args.operation == "mean":
+            val = my_utils.calculate_mean(fires)
+        elif args.operation == "median":
+            val = my_utils.calculate_median(fires)
+        elif args.operation == "std":
+            val = my_utils.calculate_std_dev(fires)
+        else:
+            print("The assigned --operation is not matched; either mean, median or std is provided.")
+            sys.exit(1)
+    print("Operation:", args.operation, val)
 
 
 if __name__ == '__main__':
