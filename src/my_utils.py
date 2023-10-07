4fe16728f88c0d0e57360a4c186ce81f85389cb2
fahsain <fahsain.nak@gmail.com>
2023-10-07 00:14:57 -0600

    followed style guide

--- src/my_utils.py
+++ src/my_utils.py
@@ -1,12 +1,16 @@
 def get_column(file_name, query_column, query_value, result_column=1):
     """
-    Extracts numeric values from a specified column in a CSV file based on a query in another column.
+    Extracts numeric values from a specified column
+    in a CSV file based on a query in another column.
 
     Parameters:
     - file_name (str): The name of the CSV file to read.
-    - query_column (int): The column number (1-based index) to use as a query filter.
-    - query_value (str): The value to search for in the specified query_column.
-    - result_column (int, optional): The column number (1-based index) from which to extract numeric values.
+    - query_column (int): The column number (1-based index)
+    to use as a query filter.
+    - query_value (str): The value to search for in
+    the specified query_column.
+    - result_column (int, optional): The column number
+    (1-based index) from which to extract numeric values.
       Defaults to 1 if not specified.
 
     Returns:
@@ -14,39 +18,38 @@ def get_column(file_name, query_column, query_value, result_column=1):
       where the query_column matches the query_value.
     """
     try:
-        f = open(file_name, 'r')  # Open the CSV file for reading.
+        f = open(file_name, 'r')
     except FileNotFoundError:
         print('Could not find', file_name)
         return None
     except PermissionError:
         print('Could not open', file_name)
         return None
-    
-    result = list()  # Initialize an empty list to store the result.
-    
+    result = list()
     for ind, line in enumerate(f):
-        array = line.rstrip().split(',')  # Split each line by ',' to get columns.
-        query = array[query_column-1]  # Get the value in the query_column.
-        
-        if query == query_value:  # Check if the query value matches the specified value.
+        array = line.rstrip().split(',')
+        query = array[query_column-1]
+        if query == query_value:
             try:
-                result.append(int(float(array[result_column-1])))  # Extract numeric data from result_column.
+                result.append(int(float(array[result_column-1])))
             except ValueError:
                 continue  # Skip if the value in result_column is not numeric.
-    
-    f.close()  # Close the file.
-    return result  # Return the list of extracted numeric values.
+    f.close()
+    return result
 
 
 def calculate_mean(lst):
     """
-    Calculates the mean (average) of a list of numeric values.
+    Calculates the mean (average) of
+    a list of numeric values.
 
     Parameters:
-    - lst (list): The list of numeric values for which you want to calculate the mean.
+    - lst (list): The list of numeric values
+    for which you want to calculate the mean.
 
     Returns:
-    - mean (float): The mean (average) value of the input list rounded to two decimal places.
+    - mean (float): The mean (average) value
+    of the input list rounded to two decimal places.
     """
     return round(sum(lst) / len(lst), 2)
 
@@ -56,10 +59,12 @@ def calculate_median(lst):
     Calculates the median of a list of numeric values.
 
     Parameters:
-    - lst (list): The list of numeric values for which you want to calculate the median.
+    - lst (list): The list of numeric values for
+    which you want to calculate the median.
 
     Returns:
-    - median (float): The median value of the input list rounded to two decimal places.
+    - median (float): The median value of the input
+    list rounded to two decimal places.
     """
     n = len(lst)
     lst.sort()
@@ -77,12 +82,14 @@ def calculate_std_dev(lst):
     Calculates the standard deviation of a list of numeric values.
 
     Parameters:
-    - lst (list): The list of numeric values for which you want to calculate the standard deviation.
+    - lst (list): The list of numeric values for
+    which you want to calculate the standard deviation.
 
     Returns:
-    - std_dev (float): The standard deviation of the input list rounded to two decimal places.
+    - std_dev (float): The standard deviation of
+    the input list rounded to two decimal places.
     """
-    mean = calculate_mean(lst)  # Calculate the mean.
-    variance = sum((xi - mean) ** 2 for xi in lst) / len(lst)  # Calculate the variance.
-    std_dev = variance ** 0.5  # Calculate the standard deviation.
+    mean = calculate_mean(lst)
+    variance = sum((xi - mean) ** 2 for xi in lst) / len(lst)
+    std_dev = variance ** 0.5
     return round(std_dev, 2)
